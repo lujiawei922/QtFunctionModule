a5ccf8da475d8ab8b8f89faa480767f9cca78ddb
lujiawei922 <635981519@qq.com>
2019-08-01 14:27:04 +0800

    二维码功能添加了在二维码底部字符功能

--- Qrcode/passwordtolabel.cpp
+++ Qrcode/passwordtolabel.cpp
@@ -1,5 +1,5 @@
-#include "passwordtolabel.h"
-#include "qrencode.h"
+﻿#include "passwordtolabel.h"
+#include "./qrencode/qrencode.h"
 
 Passwordtolabel::Passwordtolabel(QObject *parent) : QObject(parent)
 {
@@ -11,10 +11,11 @@ Passwordtolabel::~Passwordtolabel()
 
 }
 
-void Passwordtolabel::setLabelGeometry(int width,int height)
+void Passwordtolabel::setLabelGeometry(int width,int height,int strheight)
 {
-    temp_width=width; //二维码图片的大小
-    temp_height=height;
+    temp_width = width;
+    temp_height = height;
+    str_height = strheight;
 }
 
 QPixmap Passwordtolabel::GenerateQRcode(QString tempstr)
@@ -25,14 +26,14 @@ QPixmap Passwordtolabel::GenerateQRcode(QString tempstr)
     qint32 qrcode_width = qrcode->width > 0 ? qrcode->width : 1;
     //二维码图片的缩放比例,尽管老的强制类型转换也可以，但是有警告，强迫症受不了
     double scale_x = static_cast < double > (temp_width) / static_cast < double > (qrcode_width);
-    double scale_y =static_cast < double > (temp_height) /static_cast < double > (qrcode_width);
+    double scale_y =static_cast < double > (temp_height - str_height) /static_cast < double > (qrcode_width);
 
     QImage mainimg=QImage(temp_width,temp_height,QImage::Format_ARGB32);
     QPainter painter(&mainimg);
-    QColor background(Qt::white);
+    QColor background(Qt::transparent);
     painter.setBrush(background);
     painter.setPen(Qt::NoPen);
-    painter.drawRect(0, 0, temp_width, temp_height);
+    painter.drawRect(0, 0, temp_width, temp_height - str_height);
     QColor foreground(Qt::black);
     painter.setBrush(foreground);
     for( qint32 y = 0; y < qrcode_width; y ++)
@@ -47,7 +48,22 @@ QPixmap Passwordtolabel::GenerateQRcode(QString tempstr)
             }
         }
     }
+
+    //在二维码下面添加字符
+    if(str_height > 0)
+    {
+
+        painter.setPen(strcolor);
+        QFont font(family, pointSize, weight,italic);
+        painter.setFont(font);
+        QRect  mainrect= mainimg.rect();
+        mainrect.setHeight(temp_height + (font.pointSize() + 3) - str_height );
+        painter.drawText(mainrect, Qt::AlignBottom | Qt::AlignCenter,tempstr);
+    }
+
     QPixmap mainmap=QPixmap::fromImage(mainimg);
+    QRcode_free(qrcode);
+
 
     return mainmap;
 }
--- Qrcode/passwordtolabel.h
+++ Qrcode/passwordtolabel.h
@@ -1,9 +1,22 @@
-#ifndef PASSWORDTOLABEL_H
+﻿#ifndef PASSWORDTOLABEL_H
 #define PASSWORDTOLABEL_H
 
 #include <QObject>
 #include <QDebug>
 #include <QPainter>
+#include <QColor>
+
+//二维码底下字体颜色
+const QColor strcolor = Qt::red;
+//文字字体
+const QString family = "宋体";
+//设置点大小，-1为默认12
+const int pointSize = -1;
+//笔画粗细
+const int weight = QFont::Bold;
+//字体倾斜
+const bool italic = false;
+
 /**
  * @ClassName: Passwordtolabel
  * @Description: 根据字符串生成二维码
@@ -23,11 +36,11 @@ public:
     /**
      * @brief setLabelwidth ：设置二维码宽高
      * @param width : 二维码宽
-     * @param height ： 设置二维码高
+     * @param height ： 设置二维码与字符总高度
+     * @param str_height: 二维码底下字符宽度
      */
-    void setLabelGeometry(int width,int height);
+    void setLabelGeometry(int width,int height,int str_height = 0);
     QPixmap GenerateQRcode(QString tempstr);
-
 
 
 signals:
@@ -35,6 +48,7 @@ signals:
 public :
     qint32 temp_width;
     qint32 temp_height;
+    qint32 str_height;
 
 };
 
